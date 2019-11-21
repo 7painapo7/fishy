093ec7eb624ea8e72c50f9e13552db83c6711e39
7painapo7 <spurs11714@gmail.com>
2019-11-21 11:00:24 +0000

    [Add]grouid無い場合postimageのindexにいけなくした

--- app/models/regulation.rb
+++ app/models/regulation.rb
@@ -1,4 +1,6 @@
 class Regulation < ApplicationRecord
+  # validates :size, presence: true, length: { minimum: 3 },format: {/\A[0-9]+\z/}
+  # validates :count, presence: true, length: { minimum: 3 },format: {/\A[0-9]+\z/}
   validates :end_time , presence: true
   belongs_to :group
 end
