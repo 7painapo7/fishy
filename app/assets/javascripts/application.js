8efe3a82cad6ccd9d8b8a92e760feb146e1d6b49
7painapo7 <spurs11714@gmail.com>
2019-11-24 10:38:46 +0000

    [Fix]Fontとか

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+
+  /* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
